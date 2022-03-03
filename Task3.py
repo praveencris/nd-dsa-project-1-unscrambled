fa5c80ccaa8e054396093dfbcc5cdd1091e2153e
Praveen <pkchmits@gmail.com>
2022-03-03 14:47:41 +0530

    Task 3 Part A done

--- Task3.py
+++ Task3.py
@@ -4,11 +4,11 @@ It's ok if you don't understand how to read files.
 """
 import csv
 
-with open('texts.csv', 'r') as f:
+with open('D:/Praveen/FSWebDev/Python/ND-DSA/Project-1/P0/texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls.csv', 'r') as f:
+with open('D:/Praveen/FSWebDev/Python/ND-DSA/Project-1/P0/calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
@@ -34,6 +34,7 @@ Print the answer as part of a message:
  <list of codes>
 The list of codes should be print out one per line in lexicographic order with no duplicates.
 
+
 Part B: What percentage of calls from fixed lines in Bangalore are made
 to fixed lines also in Bangalore? In other words, of all the calls made
 from a number starting with "(080)", what percentage of these calls
@@ -44,3 +45,29 @@ Print the answer as a part of a message::
 to other fixed lines in Bangalore."
 The percentage should have 2 decimal digits
 """
+
+
+#Part A Solution
+
+area_codes = set()
+for call in calls:
+    if call[0].split(")")[0] == '(080':
+       if call[1].startswith('140'):
+         area_codes.add('140')
+       elif call[1].startswith("("):
+         area_codes.add("{}{}".format(call[1].split(")")[0],")"))
+       else: 
+          area_codes.add(call[1].split()[0])
+    else:
+      continue
+
+area_codes_list = list(area_codes)
+area_codes_list.sort()
+print(*area_codes_list,sep='\n')
+
+
+
+#Part B Solution
+
+
+
