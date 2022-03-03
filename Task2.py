f876451f0c95de43f747f8c5925e89067fa845fc
Praveen <pkchmits@gmail.com>
2022-03-03 13:11:44 +0530

    Task 2 Completed

--- Task2.py
+++ Task2.py
@@ -3,11 +3,11 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
 import csv
-with open('texts.csv', 'r') as f:
+with open('D:/Praveen/FSWebDev/Python/ND-DSA/Project-1/P0/texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls.csv', 'r') as f:
+with open('D:/Praveen/FSWebDev/Python/ND-DSA/Project-1/P0/calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
@@ -20,3 +20,15 @@ Print a message:
 September 2016.".
 """
 
+
+telephone_duration = {}
+for call in calls:
+    for i in range(2):
+        if telephone_duration.get(call[i]) == None:
+             telephone_duration[call[i]] = int(call[3])
+        else:
+             telephone_duration[call[i]] += int(call[3])
+            
+result = max(telephone_duration.items(),key = lambda x: x[1])
+
+print("{0} spent the longest time, {1} seconds, on the phone during September 2016.".format(result[0],result[1]))
