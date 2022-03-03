9375d66c9e8658a8bf2336863009c88777e9fa5f
Praveen <pkchmits@gmail.com>
2022-03-03 12:22:25 +0530

    Task 1 Completed

--- Task1.py
+++ Task1.py
@@ -2,12 +2,13 @@
 Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
+from __future__ import barry_as_FLUFL
 import csv
-with open('texts.csv', 'r') as f:
+with open('D:/Praveen/FSWebDev/Python/ND-DSA/Project-1/P0/texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls.csv', 'r') as f:
+with open('D:/Praveen/FSWebDev/Python/ND-DSA/Project-1/P0/calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
@@ -18,3 +19,15 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+# Create a set of all phone numbers and calculate its size as sets never have any two elements as equal
+telephone_numbers = set()
+for text in texts:
+    telephone_numbers.add('{}'.format(text[0]))
+    telephone_numbers.add('{}'.format(text[1]))
+
+for call in calls:
+    telephone_numbers.add('{}'.format(call[0]))
+    telephone_numbers.add('{}'.format(call[1]))
+
+print("There are {} different telephone numbers in the records.".format(len(telephone_numbers)))
